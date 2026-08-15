9cff51046faf102cf905b003d788f10e0e310713
Arijeet Dutta <arijeet.dutta1511@gmail.com>
2026-08-15 18:25:41 +0530

    Create config.py
    made basic ai outline

---
+++ config.py
@@ -0,0 +1,16 @@
+from groq import Groq
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+client = Groq(
+    api_key=os.getenv("GROQ_API_KEY")
+)
+
+MODEL = "llama-3.3-70b-versatile"
+
+def explain():
+    pass
+
+def handel_error():
+    pass

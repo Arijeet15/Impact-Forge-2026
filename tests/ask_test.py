d4bf4e515f6ac75ceca06c4109eb754895c2154d
Arijeet Dutta <arijeet.dutta1511@gmail.com>
2026-08-16 01:43:27 +0530

    made v1 folder in tests folder and moved the  v1 test files there

--- tests/ask_test.py
+++
@@ -1,9 +0,0 @@
-from main import ask
-
-name = ask("What is your name? ")
-age = ask("What is your age? ")
-
-print(f"You are {name} and you are {age} years old.")
-print(f"Next year you will be {age + 1} years old.")
-
-# TEST SUCESSFULL
